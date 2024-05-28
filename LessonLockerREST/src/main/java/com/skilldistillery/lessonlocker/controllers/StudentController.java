1fe9185be4e7c984f2e4b908cfcf4403f190974b
Sheldon Pasciak <sheldon@pasciak.com>
2024-05-28 17:38:18 -0600

    More quiz work

--- LessonLockerREST/src/main/java/com/skilldistillery/lessonlocker/controllers/StudentController.java
+++ LessonLockerREST/src/main/java/com/skilldistillery/lessonlocker/controllers/StudentController.java
@@ -1,6 +1,7 @@
 package com.skilldistillery.lessonlocker.controllers;
 
 import java.security.Principal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -100,6 +101,8 @@ public class StudentController {
 			newQuizAnswer.setChoice(choice);
 			newQuizAnswer.setId(quizAnswerId);
 			newQuizAnswer.setUser(user);
+			LocalDateTime now = LocalDateTime.now();
+			newQuizAnswer.setCreatedAt(now);
 
 			newQuizAnswer=quizAnswerService.create(newQuizAnswer);
 			
---
+++ ngLessonLocker/src/app/components/quizzes/quizzes.component.ts
@@ -0,0 +1,53 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { InstructorService } from '../../services/instructor.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Quiz } from '../../models/quiz';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-quizzes',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './quizzes.component.html',
+  styleUrl: './quizzes.component.css',
+})
+export class QuizzesComponent implements OnInit {
+  quizzes: Quiz[] = [];
+
+  ngOnInit(): void {
+    this.loadQuizzes();
+  }
+
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService,
+    private instructorService: InstructorService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  loadQuizzes(): void {
+    this.instructorService.loadQuizzes().subscribe({
+      next: (quizzes: Quiz[]) => {
+        console.log(quizzes);
+        if (quizzes) {
+          this.quizzes = quizzes;
+          console.log(this.quizzes);
+          return;
+        } else {
+          alert('Quizzes not found');
+          return this.router.navigateByUrl('/');
+        }
+      },
+      error: (err) => {
+        alert('You must be logged in to user this page.');
+        console.log(err?.error?.message || err?.message || err);
+        return this.router.navigateByUrl('/login');
+        return;
+      },
+    });
+  }
+}
