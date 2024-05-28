b80da033a441e68ccf9eee2a87c4fa886be8a01e
Sheldon Pasciak <sheldon@pasciak.com>
2024-05-28 07:32:56 -0600

    Small cleanup and additions

--- LessonLockerREST/src/main/java/com/skilldistillery/lessonlocker/repositories/InstructorRepository.java
+++ LessonLockerREST/src/main/java/com/skilldistillery/lessonlocker/repositories/InstructorRepository.java
@@ -3,8 +3,11 @@ package com.skilldistillery.lessonlocker.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.skilldistillery.lessonlocker.entities.Question;
+import com.skilldistillery.lessonlocker.entities.User;
 
 public interface InstructorRepository extends JpaRepository<Question, Integer> {
 	
@@ -13,5 +16,8 @@ public interface InstructorRepository extends JpaRepository<Question, Integer> {
 	List<Question> findAllByUserUsername(String username);
 	
 	List<Question> findAllByUserCohort(String cohort);
+	
+	@Query("SELECT u FROM User u WHERE u.cohort = :cohort")
+	List<User> findAllUsersByCohort(@Param("cohort") String cohort);
 
 }
